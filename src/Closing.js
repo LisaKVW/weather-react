2b18e76ee911ccb3326d0bf5a9860b30c9555ad4
LisaKVW <LisaKVW89@github.com>
2020-08-25 17:49:56 -0400

    transfered all code from sandbox to VSC

---
+++ src/Closing.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Closing.css";
+
+export default function Closing() {
+  return (
+    <div className="Closing">
+      <p className="github-link">
+        <a
+          href="https://github.com/LisaKVW/WeatherApp"
+          target="_blank"
+          className="gitlink"
+        >
+          Open-source code
+        </a>{" "}
+        by Lisa V. Wand
+      </p>
+    </div>
+  );
+}
