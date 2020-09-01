024a839af2c3dfcd01ec4be094c4f94e6dba9918
LisaKVW <LisaKVW89@github.com>
2020-09-01 19:00:53 -0400

    Header  welcome changed

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Header.css";
 
 export default function Header() {
-    return (
-        <h1>
-            {" "}
-      Good Afternoon!
-            <span className="sun_emoji" role="img" aria-label="sun">
-                {" "}
+  return (
+    <h1>
+      {" "}
+      Hello!
+      <span className="sun_emoji" role="img" aria-label="sun">
+        {" "}
         ☀️
       </span>
-        </h1>
-    );
+    </h1>
+  );
 }
