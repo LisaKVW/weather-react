96168fafbc1ff68b8daf06fbf906d03c0f40c493
LisaKVW <LisaKVW89@github.com>
2020-09-01 18:33:07 -0400

    form in Weather.js works

--- src/Weather.js
+++ src/Weather.js
@@ -5,6 +5,7 @@ import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function showTemp(response) {
     setWeatherData({
@@ -19,11 +20,32 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "75f4950e1c8c4405bfe894443eefc1e1";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(showTemp);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function showSearchCity(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form className="form-inline">
-          <input type="text" placeholder="Search city" autoFocus="on" />
+        <form className="form-inline" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search city"
+            autoFocus="on"
+            onChange={showSearchCity}
+          />
           <input type="submit" value="search" />
           <button type="button" id="location-emoji">
             <span
@@ -39,11 +61,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "75f4950e1c8c4405bfe894443eefc1e1";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(showTemp);
-
+    search();
     return "loading...";
   }
 }
