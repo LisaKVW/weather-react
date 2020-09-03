90837a0c69c1477602887ccdcd7be60feb2eaf2b
LisaKVW <LisaKVW89@github.com>
2020-09-03 17:11:13 -0400

    layout adjusted WeatherInfo + Header and Closing

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -14,13 +14,17 @@ export default function WeatherForecast(props) {
 
   if (loaded && props.city === forecast.city.name) {
     return (
-      <div className="WeatherForecast row">
-        <WeatherForecastPreview data={forecast.list[0]} />
-        <WeatherForecastPreview data={forecast.list[1]} />
-        <WeatherForecastPreview data={forecast.list[2]} />
-        <WeatherForecastPreview data={forecast.list[3]} />
-        <WeatherForecastPreview data={forecast.list[4]} />
-      </div>
+      <span className="WeatherForecast">
+        {" "}
+        Upcoming weather for your location:
+        <div className="WeatherForecast row">
+          <WeatherForecastPreview data={forecast.list[0]} />
+          <WeatherForecastPreview data={forecast.list[1]} />
+          <WeatherForecastPreview data={forecast.list[2]} />
+          <WeatherForecastPreview data={forecast.list[3]} />
+          <WeatherForecastPreview data={forecast.list[4]} />
+        </div>
+      </span>
     );
   } else {
     let apiKey = "75f4950e1c8c4405bfe894443eefc1e1";
